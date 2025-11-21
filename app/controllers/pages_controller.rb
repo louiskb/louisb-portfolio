7f28719e9d73d23baa3a8b01b6432f16717435ce
Louis Bourne <chifury@proton.me>
2025-11-22 03:08:01 +0700

    stle: added open source projects section to the featured projects partial.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,12 +13,30 @@ class PagesController < ApplicationController
       },
       {
         title: "Water",
-        description: "An app about how cool water is.",
-        img_url: "tech-city",
+        description: "An app about how cool water is. It's amazing and has many features just to learn about water and a h20 for hydration.",
+        img_url: "dark-waves",
         tech_stack: "Ruby-on-Rails . JavaScript . React . PostgreSQL",
         project_url: "https://www.sipfolio.rocks",
         github_url: "https://github.com/chifury/rails-mister-cocktail"
       },
+      {
+        title: "Fire",
+        description: "An app about how cool fire is. It's amazing and has many features just to learn about fire and energy for civilization.",
+        img_url: "dark-shapes",
+        tech_stack: "Ruby-on-Rails . JavaScript . React . PostgreSQL",
+        project_url: "https://www.sipfolio.rocks",
+        github_url: "https://github.com/chifury/rails-mister-cocktail"
+      }
+    ]
+    @open_source = [
+      {
+        title: "Find a Doc Japan",
+        description: "An app that connects expats living in Japan with the right doctors.",
+        img_url: "cosmos-gaze",
+        tech_stack: "Vue.js . JavaScript . PostgreSQL",
+        project_url: "https://www.findadoc.jp",
+        github_url: "https://github.com/ourjapanlife"
+      }
     ]
   end
 end
