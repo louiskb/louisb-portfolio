3de31cad93aa90de21ddbc2ef5ef4027ad501de2
Louis Bourne <chifury@proton.me>
2025-11-22 01:50:35 +0700

    style: created a vertical social bar for the hero banner.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,23 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @projects = [
+      {
+        title: "Sipfolio",
+        description: "A social cocktail app where users discover, create, and share AI-enhanced recipes, with gamified rewards to drive engagement.",
+        img_url: "dark-waves",
+        tech_stack: "Ruby-on-Rails . JavaScript . Ruby . PostgreSQL",
+        project_url: "https://www.sipfolio.rocks",
+        github_url: "https://github.com/chifury/rails-mister-cocktail"
+      },
+      {
+        title: "Water",
+        description: "An app about how cool water is.",
+        img_url: "tech-city",
+        tech_stack: "Ruby-on-Rails . JavaScript . React . PostgreSQL",
+        project_url: "https://www.sipfolio.rocks",
+        github_url: "https://github.com/chifury/rails-mister-cocktail"
+      },
+    ]
   end
 end
