21a2a51743b443b4c60e5472076292a75a3b0d7d
Louis Bourne <chifury@proton.me>
2025-12-06 10:47:53 +0700

    fix: added new honeypot field names to prevent browser auto-complete affecting humans.

--- config/initializers/invisible_captcha.rb
+++ config/initializers/invisible_captcha.rb
@@ -1,6 +1,6 @@
 InvisibleCaptcha.setup do |config|
   config.timestamp_threshold = 5
-  config.honeypots = ["nickname", "phone"]
+  config.honeypots = ["nickname", "website_url", "company", "subject"]
   # `invisible_captcha` always logs spam blocks to log/development.log. In development: Check log/development.log in your Rails root run in terminal: `tail -f log/development.log | grep "InvisibleCaptcha"`. Live on Heroku run: `heroku logs --tail | grep InvisibleCaptcha`.
   # Refer to `invisible_captcha` README https://github.com/markets/invisible_captcha
 end
