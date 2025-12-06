12a11687f5d2a0bb9e52dd6d8937b1478fee034f
Louis Bourne <chifury@proton.me>
2025-12-06 16:54:46 +0700

    feat: added code explanation comments in the ContactMailer class to potentially refer to later. Removed html_safe view helper from the received_email.html.erb view for improved email security.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,9 @@
 class ContactMailer < ApplicationMailer
 
+  # What is a Rails mailer class ?
+  # Mailer classes generated with the BASH command eg. `rails generate mailer ContactMailer received_email confirmation_email`, generates a new Rails mailer class in this case called `ContactMailer` and starter templates for two email actions.
+  # Mailer classes generated with this command eg. `rails generate mailer ContactMailer` (ie. contact_mailer.rb / ContactMailer class) defines (1) what emails are sent (the mailer actions (methods)), (2) to whom and what subject (headers of the email eg. to, from, subject, reply_to), and (3) content (matching views that define the content, look, and structure of the email sent) for your contact form.
+
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
