8213732cb579d2d7390dc72987811bffc94ab5d6
Louis Bourne <chifury@proton.me>
2025-12-14 11:29:23 +0700

    style: updated navbar styles to be more mobile UX friendly.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
   def nav_link_class(path = "#")
-    current_page?(path) ? "nav-link active" : "nav-link"
+    current_page?(path) ? "nav-link active fs-5" : "nav-link fs-5"
   end
 
   def nav_link_dropdown_class(path = "#")
-    current_page?(path) ? "dropdown-item active" : "dropdown-item"
+    current_page?(path) ? "dropdown-item active fs-6" : "dropdown-item fs-6"
   end
 end
